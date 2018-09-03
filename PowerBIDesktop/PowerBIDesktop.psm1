9da63533923bf5b9c1b3f2ec9dd91d8d07d37cb0
Giles Antonio Radford <garadford@transcoma.com>
2018-09-03 14:37:38 +0200

    Renamed Invoke-PowerBIDesktopDAXCommand to Invoke-PowerBIDesktopCommand Added Documentation for  Invoke-PowerBIDesktopCommand Added Get-PowerBIDesktopTables

--- PowerBIDesktop/PowerBIDesktop.psm1
+++ PowerBIDesktop/PowerBIDesktop.psm1
@@ -51,14 +51,14 @@ Port       : 51125
     # Now we decorate the process objects:
     $pbiProcesses | ForEach-Object {Add-Member -InputObject $_ -NotePropertyName Port -NotePropertyValue ($pbiports | Where-Object OwningProcess -eq $_.Id).RemotePort}
     $pbiProcesses | ForEach-Object {Add-Member -InputObject $_ -NotePropertyName Address -NotePropertyValue ($pbiports | Where-Object OwningProcess -eq $_.Id).RemoteAddress}
-    $pbiProcesses | ForEach-Object {Add-Member -InputObject $_ -NotePropertyName DataSource -NotePropertyValue ($_.Address, ":", $_.Port -Join "")}
-    #$pbiProcesses | ForEach-Object {Add-Member -InputObject $_ -NotePropertyName ConnectionString -NotePropertyValue ($_.Address, ":", $_.Port -Join "")}
+    # This used to be $_.Address:$_.Port but it appears that ADOMD.Net doesn't support addresses of type "::1:23456" for the data source parameter
+    $pbiProcesses | ForEach-Object {Add-Member -InputObject $_ -NotePropertyName DataSource -NotePropertyValue "localhost:$($_.Port)"}
     $pbiProcesses | ForEach-Object {Add-Member -InputObject $_ -NotePropertyName Title -NotePropertyValue $_.MainWindowTitle.Replace(" - Power BI Desktop", "")}
     
     # Remove unneeded information
     $pbiProcesses = $pbiProcesses | Select-Object Id, Title, DataSource, Address, Port
 
-    if ($title -ne $null) {
+    if ($title) {
         $pbiProcesses | Where-Object Title -Like $Title
     }
     else {
@@ -68,7 +68,35 @@ Port       : 51125
 
 }
 
-Function Invoke-PowerBIDesktopDAXCommand {
+Function Invoke-PowerBIDesktopCommand {
+<#
+.SYNOPSIS
+Run a command on a Power BI Desktop Analysis Server session.
+.Description
+Run a command on a Power BI Desktop Analysis Server session.
+This cmdlet allows you to pass a DAX, MDX or ASSL (XMLA) command on a running Power BI Desktop backend.
+Outputs a dataset containing the result of the command.
+Use -Title to specify the window title of the Power BI Desktop instance that you want to connect to, if there is more than one Power BI Desktop window open.
+
+Power BI Desktop internally runs queries against a modified version of SQL Server Analysis Services, and this cmdlet connects to it using ADOMD.Net.
+It creates a Dataset that connects to the query result, and then returns the first table from its Tables collection.
+
+For more information on DAX: https://msdn.microsoft.com/query-bi/dax/dax-queries
+For more information on MDX: https://docs.microsoft.com/en-us/sql/analysis-services/multidimensional-models/mdx/mdx-query-fundamentals-analysis-services
+For more information on ASSL (XMLA): https://docs.microsoft.com/en-us/sql/analysis-services/scripting/analysis-services-scripting-language-assl-for-xmla
+.PARAMETER Command
+The Command to run
+.PARAMETER Title
+The window title of the Power BI Desktop instance to run it against. 
+This can contain wild cards: "Fabrikam*" or "*eports*"
+
+This is not needed if there is only one Power BI Desktop instance running
+.EXAMPLE
+Retrieve the tables in the only running Power BI Dekstop instance
+
+
+#>
+
     [CmdletBinding()]
 
     param(
@@ -76,10 +104,15 @@ Function Invoke-PowerBIDesktopDAXCommand {
         [string]
         $Command,
         [string]
-        $Title = "*"
+        $Title
     )
 
-    $sessions = Get-PowerBIDesktopConnections -Title $Title
+    if ($title) {
+        $sessions = Get-PowerBIDesktopConnections -Title $Title
+    } else {
+        $sessions = Get-PowerBIDesktopConnections
+    }
+    
 
     if ($sessions.length -eq 0) {
         if ($Title) {
@@ -104,11 +137,11 @@ Function Invoke-PowerBIDesktopDAXCommand {
         [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.AnalysisServices.AdomdClient")  
         $connection = New-Object Microsoft.AnalysisServices.AdomdClient.AdomdConnection
     } catch {
-        Write-Error -ErrorAction Stop -Message "Could not create the connection. ADOMD.NET not installed. Please go to https://www.microsoft.com/en-us/download/details.aspx?id=52676 and download and install SQL_AS_ADOMD.msi"
+        Write-Error -ErrorAction Stop -Message "Could not create the connection. ADOMD.NET not installed. Please go to https://www.microsoft.com/en-us/download/details.aspx?id=52676 and download and install SQL_AS_ADOMD.msi or using nuget install Microsoft.AnalysisServices.AdomdClient.retail.amd64"
     }
 
     try {
-        $connection.ConnectionString = "Data Source=localhost:$($session.port)"
+        $connection.ConnectionString = "Data Source=$($session.DataSource)"
         $connection.open()
     } catch {
         Write-Error "Error connecting to $($session.Title) [$($connection.ConnectionString)]"
@@ -116,7 +149,7 @@ Function Invoke-PowerBIDesktopDAXCommand {
     }
 
     try {
-        Write-Verbose "Executing the DAX command: $Command"
+        Write-Verbose "Executing the Power BI Desktop command: $Command"
         $com = $connection.CreateCommand()
         $com.CommandText = "$Command"
         $adapter = New-Object -TypeName Microsoft.AnalysisServices.AdomdClient.AdomdDataAdapter $com
@@ -125,9 +158,48 @@ Function Invoke-PowerBIDesktopDAXCommand {
         $adapter.Fill($dataset)
         
     } catch {
-        Write-Error "Error Executing DAX Statement: $Command"
+        Write-Error "Error Executing Power BI Desktop command: $Command"
         throw $_
     }
     
+    $connection.close()
+
     $dataset
+}
+
+Function Get-PowerBIDesktopTables {
+<#
+.SYNOPSIS
+Gets the list of available tables in a Power BI Desktop Session and their descriptions
+.Description
+Gets the list of available tables in a Power BI Desktop Session and their descriptions
+If there is more than one Power BI Desktop Session active, you can specify the one you want to query with -Title
+There may be tables that are hidden from the normal Power BI interface. You can show them with -IncludeHidden
+.PARAMETER Title
+The window title of the Power BI Desktop instance to run it against. 
+This can contain wild cards: "Fabrikam*" or "*eports*"
+
+This is not needed if there is only one Power BI Desktop instance running
+.PARAMETER IncludeHidden
+This will include the tables that are hidden in the normal Power BI Desktop Interface
+#>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$false)]
+        [string]
+        $Title = $null,
+        # Parameter help description
+        [Parameter(Mandatory=$false)]
+        [Switch]
+        $IncludeHidden
+    )
+
+    
+    $dataset = Invoke-PowerBIDesktopCommand "SELECT * FROM `$SYSTEM.TMSCHEMA_TABLES" -Title $Title
+    
+    if ($IncludeHidden) {
+        $dataset.Tables[0] | Select-Object Name, Description
+    } else {
+        $dataset.Tables[0] | where-object {$_.IsHidden -eq $false } | Select-Object Name, Description
+    }
 }
